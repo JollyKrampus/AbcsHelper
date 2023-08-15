fb7356a07392793db7da1a5c1ad36ff8d4013a2c
Trooy Thompson <tr-09@hotmail.com>
2023-08-15 11:00:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Letter Recognition Trainer for Kindergarteners
+
+A web-based application designed to help young learners swiftly identify and respond to both uppercase and lowercase letters of the alphabet. With real-time feedback, this tool aids in enhancing letter recognition skills, making it an ideal fit for kindergarteners and early readers.
+
+## Features
+
+- **Random Letter Generation**: Displays letters in random order, switching between uppercase and lowercase.
+- **Real-time Feedback**: Instantly tracks and displays the count of correctly identified and wrongly identified letters.
+- **Simple Interface**: Clean, distraction-free interface ensures that the learner stays focused on the task.
+
+## Getting Started
+
+Just navigate to the hosted GitHub Pages link to start practicing!
+
+### Local Development
+
+If you wish to run or modify the app locally:
+
+1. Clone this repository.
+`git clone https://github.com/JollyKrampus/AbcsHelper.git`
+2. Open `index.html` in your preferred browser.
+3. Start practicing!
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ script.js
@@ -0,0 +1,65 @@
+let correctLetters = [];
+let wrongLetters = [];
+let allLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+let startTime;
+let endTime;
+
+function generateRandomLetter() {
+    if (allLetters.length === 0) {
+        endTime = new Date();
+        finishQuiz();
+        return;
+    }
+    
+    const index = Math.floor(Math.random() * allLetters.length);
+    const randomLetter = allLetters[index];
+    document.getElementById('random-letter').textContent = randomLetter;
+
+    // Remove the displayed letter from allLetters
+    allLetters.splice(index, 1);
+}
+
+function markCorrect() {
+    const letter = document.getElementById('random-letter').textContent;
+    correctLetters.push(letter);
+    document.getElementById('correct-letters').textContent = correctLetters.join(', ');
+    generateRandomLetter();
+}
+
+function markWrong() {
+    const letter = document.getElementById('random-letter').textContent;
+    wrongLetters.push(letter);
+    document.getElementById('wrong-letters').textContent = wrongLetters.join(', ');
+    generateRandomLetter();
+}
+
+function disableButtons() {
+    document.getElementById('btn-correct').disabled = true;
+    document.getElementById('btn-wrong').disabled = true;
+}
+
+function finishQuiz() {
+    disableButtons();
+    
+    const total = correctLetters.length + wrongLetters.length;
+    const percentage = ((correctLetters.length / total) * 100).toFixed(2);
+    document.getElementById('percentage-correct').textContent = `${percentage}%`;
+
+    const timeDiff = (endTime - startTime) / 1000;  // Convert to seconds
+    document.getElementById('time-taken').textContent = `${timeDiff} seconds`;
+
+    // Sort and display the letters
+    document.getElementById('correct-letters').textContent = correctLetters.sort().join(', ');
+    document.getElementById('wrong-letters').textContent = wrongLetters.sort().join(', ');
+
+    // Update the final counts
+    document.getElementById('correct-count-final').textContent = correctLetters.length;
+    document.getElementById('wrong-count-final').textContent = wrongLetters.length;
+
+    // Show the results div
+    document.getElementById('results').style.display = 'block';
+}
+
+// Initialize with the first random letter
+startTime = new Date();
+generateRandomLetter();
